956bbb9169fe29d6ca65132d3ae6b96ddf7e605f
Joonkyu Park <joonkyu4220@kaist.ac.kr>
2022-11-17 13:54:51 +0900

    formatting and simulation time control

--- test_policy.py
+++ test_policy.py
@@ -1,66 +1,54 @@
 if __name__ == '__main__':
-   import json
-   from ruamel.yaml import YAML, dump, RoundTripDumper
-   from raisimGymTorch.env.bin import deepmimic
-   from raisimGymTorch.env.RaisimGymVecEnv import RaisimGymVecTorchEnv as VecEnv
-   from raisimGymTorch.helper.raisim_gym_helper import ConfigurationSaver, load_param, tensorboard_launcher
- 
-   import torch
-   import torch.optim as optim
-   import torch.multiprocessing as mp
-   import torch.nn as nn
-   import torch.nn.functional as F
-   from torch.autograd import Variable
-   import torch.utils.data
-   from model import ActorCriticNet, ActorCriticNetMann
-   import os
-   import numpy as np
-   device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    import time; 
+    from ruamel.yaml import YAML, dump, RoundTripDumper
+    from raisimGymTorch.env.bin import deepmimic
+    from raisimGymTorch.env.RaisimGymVecEnv import RaisimGymVecTorchEnv as VecEnv
 
-   from tkinter import *
-   from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-   from matplotlib.figure import Figure
-   import matplotlib.pyplot as plt
- 
-   seed = 3#8
-   torch.manual_seed(seed)
-   torch.cuda.manual_seed(seed)
-   torch.set_num_threads(1)
- 
-   # directories 
-   task_path = os.path.dirname(os.path.realpath(__file__))
-   home_path = task_path + "/../../../../.."
-   # print(task_path)
-   # print(home_path)
+    import torch
+    import torch.utils.data
+    from model import ActorCriticNet
+    import os
+    import numpy as np
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-   model_path = task_path + "/stats/20221107_final"
+    seed = 3#8
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.set_num_threads(1)
+    
+    # directories 
+    task_path = os.path.dirname(os.path.realpath(__file__))
+    home_path = task_path + "/../../../../.."
 
-   # config
-   cfg = YAML().load(open(model_path + "/cfg.yaml", 'r'))
+    model_path = task_path + "/stats/20221107_final"
 
-   # create environment from the configuration file
-   env = VecEnv(deepmimic.RaisimGymEnv(home_path + "/rsc", dump(cfg['environment'], Dumper=RoundTripDumper)), cfg['environment'])
+    # config
+    cfg = YAML().load(open(model_path + "/cfg.yaml", 'r'))
 
-   print("env_created")
+    # create environment from the configuration file
+    env = VecEnv(deepmimic.RaisimGymEnv(home_path + "/rsc", dump(cfg['environment'], Dumper=RoundTripDumper)), cfg['environment'])
 
-   num_inputs = env.observation_space.shape[0]
-   num_outputs = env.action_space.shape[0]
-   # model = ActorCriticNetMann(num_inputs, num_outputs, [128, 128])
-   model = ActorCriticNet(num_inputs, num_outputs, [128, 128])
-   model.load_state_dict(torch.load(model_path + "/iter49999.pt"))
-   model.cuda()
+    print("env_created")
 
-   num_frames = 3000
-   save_data = np.zeros((num_frames, env.num_states))
+    num_inputs = env.observation_space.shape[0]
+    num_outputs = env.action_space.shape[0]
+    # model = ActorCriticNetMann(num_inputs, num_outputs, [128, 128])
+    model = ActorCriticNet(num_inputs, num_outputs, [128, 128])
+    model.load_state_dict(torch.load(model_path + "/iter49999.pt"))
+    model.cuda()
 
-   env.setTask()
-   
-   env.reset()
-   
-   obs = env.observe()
-   
-   for i in range(num_frames):
-      with torch.no_grad():
-         act = model.sample_best_actions(obs)
-      obs, rew, done, _ = env.step(act)
-      import time; time.sleep(0.02)
+    env.setTask()
+    
+    env.reset()
+    
+    obs = env.observe()
+
+    max_length = 100000
+    
+    control_dt = float(cfg["environment"]["control_dt"])
+    
+    for i in range(max_length):
+        with torch.no_grad():
+            act = model.sample_best_actions(obs)
+        obs, rew, done, _ = env.step(act)
+        time.sleep(control_dt)
